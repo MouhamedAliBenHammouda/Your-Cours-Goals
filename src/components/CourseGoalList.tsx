2d04f56c59cf26770954f9bd39e41019176c944d
MouhamedAliBenHammouda <mouhamedalibenhammouda7@gmail.com>
2024-04-15 15:26:54 +0100

    v1

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,23 +1,40 @@
 import CoursGoal from "./CoursGoal";
 import { type CourseGoal } from "../App";
+import InfoBox from "./infoBox";
+import { type ReactNode } from "react";
 
 type CoursGoalListProps = {
     goals: CourseGoal[];
-    onDeleteGoal:(id:number)=> void;
+    onDeleteGoal: (id: number) => void;
 };
 
-export default function CourseGoalList({ goals,onDeleteGoal }: CoursGoalListProps) {
+export default function CourseGoalList({ goals, onDeleteGoal }: CoursGoalListProps) {
+    if (goals.length === 0) {
+        return <InfoBox mode="hint">You have no course goals yet.Startbadding some!</InfoBox>
+    }
+    let waringBox: ReactNode;
+
+    if(goals.length>=4){
+        waringBox=(
+            <InfoBox mode="warning">
+            
+            </InfoBox>
+        )
+    }
     return (
-        <ul>
-            {goals.map((goal) => <li key={goal.id}>
-                <CoursGoal
-                    id={goal.id}
-                    onDelete={onDeleteGoal}
-                    title={goal.title}
+        <>
+
+            <ul>
+                {goals.map((goal) => <li key={goal.id}>
+                    <CoursGoal
+                        id={goal.id}
+                        onDelete={onDeleteGoal}
+                        title={goal.title}
                     >
-                    {goal.desctiption}
-                </CoursGoal>
-            </li>)}
-        </ul>
+                        {goal.desctiption}
+                    </CoursGoal>
+                </li>)}
+            </ul>
+        </>
     )
 }
